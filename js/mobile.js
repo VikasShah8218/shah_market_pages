53d56fcae588c9611ae221483d36ea1ec33937be
Mr. Shah <vikasshah8218@gmail.com>
2023-10-03 19:16:36 +0530

    Testing_pages

---
+++ js/mobile.js
@@ -0,0 +1,16 @@
+// const doc = document;
+// const menuOpen = doc.querySelector(".menu");
+// const menuClose = doc.querySelector(".close");
+// const overlay = doc.querySelector(".overlay");
+
+// document.getElementById("ok").addEventListener("click", () => {
+//   overlay.classList.add("overlay--active");
+// });
+
+// menuClose.addEventListener("click", () => {
+//   overlay.classList.remove("overlay--active");
+// });
+
+// const a  = document.getElementById("ok");
+// console.log("****************************************")
+// console.log(document.getElementById("ok").innerText)
